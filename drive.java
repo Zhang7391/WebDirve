e80de3d2e3aa334c0720c5d6a018ea1c86061b9b
zhang7391 <zhang7391@protonmail.com>
2023-02-14 03:15:16 +0800

    Check out webpage's cookie more easier.

--- drive.java
+++ drive.java
@@ -1,13 +1,9 @@
 package taiwan.webpage;
 
-import javax.servlet.http.Cookie;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.request.http.WebRequest;
-import org.apache.wicket.request.http.WebResponse; 
 import org.wicketstuff.annotation.mount.MountPath;
-import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -23,96 +19,52 @@ import taiwan.formwork.html.Copyright;
 @MountPath("/drive")
 public class drive extends WebPage
 {
-	private String username = null;
-
 	public drive()
 	{
 		super();
 
-		boolean key = this.cookieChecker();
-		if(key) 
+		LoginCheck loginCheck = new LoginCheck((WebPage)this);
+		
+		boolean key = true;
+		if(!loginCheck.verify()) 
 		{
-			PageParameters pageParameters = new PageParameters();
-			pageParameters.add("codename", this.username);
-
-			this.setResponsePage(new drive(pageParameters));
+			key = false;
+			this.setResponsePage(login.class);
 		}
-		else this.setResponsePage(login.class);
+
+		if(key) loginCheck.reply(this.getPageClass());
 	}
 
 	public drive(PageParameters pageParameters) 
 	{
 		super(pageParameters);
 
-		boolean key = this.cookieChecker();
-		
-		if(key && pageParameters.get("codename").isNull())
+		LoginCheck loginCheck = new LoginCheck((WebPage)this);
+
+		boolean key = true;
+		if(!loginCheck.verify())
 		{
 			key = false;
 			this.setResponsePage(login.class);
 		}
+
+		if(key && !pageParameters.get("codename").toString().contentEquals(loginCheck.getUsername()))
+			key = loginCheck.reply(this.getPageClass());
 		
 		if(key) 
 		{
-			pageParameters.add("user", pageParameters.get("codename").toString());
 			add(new Titlebar("titlebar", "\u500b\u4eba\u96f2\u7aef\u786c\u789f", true, pageParameters, operator.class));
-			pageParameters.remove("user");
 
 			add(new Copyright("copyright"));
 		}
-		else this.setResponsePage(login.class);
 	}
-
-	private boolean cookieChecker()
-	{
-		WebRequest request = (WebRequest) RequestCycle.get().getRequest();
-		Cookie cookie = request.getCookie("passingID");
-
-		boolean key = true;
-		
-		LoginCheck loginCheck = new LoginCheck((WebPage)this, cookie);
-		this.username = loginCheck.getUsername();
-		
-		if(cookie != null)
-		{
-			if(!loginCheck.logging())
-			{
-				cookie.setMaxAge(0);
-
-				WebResponse webResponse = (WebResponse)RequestCycle.get().getResponse();
-				webResponse.addCookie(cookie);
-
-				key = false;
-			}
-			
-//			key = loginCheck.reply(index.class, "user");
-		}
-		else key = false;
-
-		return key;
-	}
-
-/*	public drive(PageParameters pageParameters, boolean lawful)
-	{
-		if(lawful) 
-		{
-			add(new Titlebar("titlebar", "\u9996\u9801", true, pageParameters, this.getPageClass()));
-
-			add(new Copyright("copyright"));
-			
-			add(new Label("text", "Logined.").setRenderBodyOnly(true));
-		}
-	}*/
 
 	@Override
 	public void renderHead(IHeaderResponse response)
 	{
-		Url url = RequestCycle.get().getRequest().getUrl();
-		String fullUrl = RequestCycle.get().getUrlRenderer().renderFullUrl(url);
-
-		String rootUrl;
-		try {rootUrl = fullUrl.substring(0, fullUrl.indexOf('/', fullUrl.indexOf('/')+2));}
-		catch(Exception error) {rootUrl = fullUrl;}
+		Url url = this.getRequestCycle().get().getRequest().getUrl();
+		String fullUrl = this.getRequestCycle().get().getUrlRenderer().renderFullUrl(url);
+		String rootUrl = fullUrl.substring(0, fullUrl.indexOf('/', fullUrl.indexOf('/')+2));
 
 		response.render(CssReferenceHeaderItem.forUrl(rootUrl + "/global.css"));
 		response.render(CssReferenceHeaderItem.forUrl(rootUrl + "/css/index.css"));
