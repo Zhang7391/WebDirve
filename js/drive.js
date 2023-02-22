a0b4288275b4889556c4e9380ff9af8a31832943
fradaut <qoo19830526@gmail.com>
2023-02-22 17:24:21 +0800

    新增檔案重複檢查, 新增錯誤視窗 Add file duplication check, add error window.

--- js/drive.js
+++ js/drive.js
@@ -45,12 +45,29 @@ window.addEventListener("DOMContentLoaded", () =>
         for (let i = 0; files.length > i; i++) 
 		{
             const file = files[i];
+
+            let check = false;
+            for (let j=0; j < document.querySelectorAll("#filelist").length; j++)
+            {
+                if (file["name"] === document.querySelectorAll("#filelist")[j].innerText){
+                    check = true;
+                    break;
+                }
+            }
+            if (check)
+            {
+                check = false;
+                failWindow("檔案重複");
+                break;
+            }
+
             const tr = document.createElement("tr");
             tr.classList.add("obj_tr");
 
             const td = document.createElement("td");
             td.innerText = file["name"];
             td.classList.add("obj_td");
+            td.setAttribute("id", "filelist");
 
             const td2 = document.createElement("td");
             td2.innerText = "-";
@@ -79,10 +96,27 @@ window.addEventListener("DOMContentLoaded", () =>
         handleFiles(files);
     }
 
+    function failWindow(text)
+    {
+        for (let i=0; i<document.querySelectorAll("#disposable").length; i++) {
+            document.querySelectorAll("#disposable")[i].remove();
+        }
+        let content = document.createElement("div");
+        content.textContent = text;
+        content.setAttribute("id", "disposable");
+        document.querySelector(".upload_failed").appendChild(content);
+        document.querySelector(".upload_failed").classList.remove("display_none");
+    }
+
+    const failWindowButton = document.querySelector(".failed_btn");
+    const failWindowButtonClick = e => document.querySelector(".upload_failed").classList.add("display_none");
+
 	// true is capturing
 	// false is bubbling
     dropbox.addEventListener("dragenter", dragenter, false);
     dropbox.addEventListener("dragleave", dragleave, false);
     dropbox.addEventListener("dragover", dragover, false);
     dropbox.addEventListener("drop", drop, false);
+
+    failWindowButton.addEventListener("click", failWindowButtonClick, false);
 });
